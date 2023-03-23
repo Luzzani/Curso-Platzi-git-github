89d3bd55871b5e65dad999edc6c9ae62080ea732
Lucas Luzzani <lucasluzzani@gmail.com>
2023-03-23 16:25:40 -0300

    creando un alias y un tag

---
+++ index.js
@@ -0,0 +1,31 @@
+const daummyData = [
+  {
+    name: "Howard Williamson",
+    email: "etiam@hotmail.couk",
+    country: "Turkey",
+  },
+  {
+    name: "Hermione Barron",
+    email: "turpis.aliquam.adipiscing@icloud.org",
+    country: "Australia",
+  },
+  {
+    name: "Reagan Hale",
+    email: "dapibus.ligula@protonmail.ca",
+    country: "Russian Federation",
+  },
+  {
+    name: "Aaron Wong",
+    email: "tempus.lorem@hotmail.org",
+    country: "Sweden",
+  },
+  {
+    name: "Slade Baldwin",
+    email: "etiam.gravida@yahoo.ca",
+    country: "Russian Federation",
+  },
+];
+
+const result = data.map((obj) => {
+  console.log(obj.name);
+});
